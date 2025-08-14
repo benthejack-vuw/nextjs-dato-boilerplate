bc6fa96420b4a008e2c857b7d71ce7eba3a6e778
Ben Jack <benthejack@gmail.com>
2025-08-14 19:46:37 +0100

    remove comments

--- src/app/api/invalidate-cache/route.tsx
+++ src/app/api/invalidate-cache/route.tsx
@@ -7,22 +7,6 @@ import {
 	successfulResponse,
 } from "../utils";
 
-/*
- * This route handler receives "Cache Tag Invalidation" events from a DatoCMS
- * webhook, and is responsible for invalidating the Next.js cache.
- *
- * Please note, this project uses a very basic cache invalidation method: all
- * requests to DatoCMS are tagged with "datocms" in the Next.js Data Cache.
- * Whenever DatoCMS notifies us of any updates, we invalidate all requests with
- * the same tag.
- *
- * Although this caching strategy may be sufficient for smaller websites, it is
- * not advised for larger projects. Fortunately, with DatoCMS and Next, it is
- * possible to implement a much more detailed invalidation strategy.
- *
- * For more information: https://www.datocms.com/docs/next-js/using-cache-tags
- */
-
 export async function POST(req: NextRequest): Promise<NextResponse> {
 	try {
 		// Parse query string parameters
--- src/graphql/graphql-request.tsx
+++ src/graphql/graphql-request.tsx
@@ -28,32 +28,3 @@ export function datoRequest<TQuery, TVariables>(
 		]),
 	);
 }
-
-// export async function gqlRequest<TQuery, TVariables>(
-// 	query: TypedDocumentNode<TQuery, TVariables>,
-// 	variables?: TVariables,
-// 	draftMode?: boolean,
-// ) {
-// 	const response = await fetch("https://graphql.datocms.com", {
-// 		method: "POST",
-// 		headers: {
-// 			"Content-Type": "application/json",
-// 			Accept: "application/graphql-response+json",
-// 			Authorization: process.env.DATO_RO_TOKEN as string,
-// 			...(process.env.DATO_ENVIRONMENT
-// 				? { "X-Environment": process.env.DATO_ENVIRONMENT as string }
-// 				: {}),
-// 			...(draftMode ? { "X-Include-Drafts": "true" } : {}),
-// 		},
-// 		body: JSON.stringify({
-// 			query,
-// 			variables,
-// 		}),
-// 	});
-//
-// 	if (!response.ok) {
-// 		throw new Error("Network response was not ok");
-// 	}
-//
-// 	return response.json() as TQuery;
-// }
